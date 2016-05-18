452c92cff329caa202cb7a5278b9a0e754f501b2
rbn42 <bl100@students.waikato.ac.nz>
2016-05-19 10:20:00 +1200

    ignore command list

--- start.py
+++ start.py
@@ -1,15 +1,22 @@
 from mpd import MPDClient
 client = MPDClient()               # create client object
-client.timeout = 10                # network timeout in seconds (floats allowed), default: None
-client.idletimeout = None          # timeout for fetching the result of the idle command is handled seperately, default: None
+# network timeout in seconds (floats allowed), default: None
+client.timeout = 10
+# timeout for fetching the result of the idle command is handled
+# seperately, default: None
+client.idletimeout = None
 client.connect("localhost", 6600)  # connect to localhost:6600
 print(client.mpd_version)          # print the MPD version
-print(client.find("any", "house")) # print result of the command "find any house"
-client.command_list_ok_begin()       # start a command list
+# print result of the command "find any house"
+print(client.find("any", "house"))
+# command list的机制现在还不理解,在把python-mpd2的动态方法改成静态方法的过程中,command list暂且还不能好好处理
+# client.command_list_ok_begin()       # start a command list
 client.update()                      # insert the update command into the list
 client.status()                      # insert the status command into the list
-results = client.command_list_end()  # results will be a list with the results
-print((results))
+print(client.status())
+print(client.stats())
+# results = client.command_list_end()  # results will be a list with the results
+# print((results))
 client.iterate = True
 for song in client.playlistinfo():
     #print( song["file"])
@@ -21,4 +28,4 @@ print(events)
 print(client.status())
 client.close()                     # send the close command
 client.disconnect()                # disconnect from the server
-#client.delete((1,))     # delete all songs, but the first.
+# client.delete((1,))     # delete all songs, but the first.
