db2fd999293e95a065dbec52f647bed5c5869ba2
rbn42 <bl100@students.waikato.ac.nz>
2016-07-22 18:40:33 +1200

    dislike.

--- dislike.py
+++ dislike.py
@@ -1,10 +1,10 @@
 import util
-client=util.startClient()
-#output identity to record this song
-song=client.currentsong()
-#identity={'file':song.get('file'),'track':song.get('track')}
-#print(identity)
+client = util.startClient()
+# output identity to record this song
+song = client.currentsong()
+# identity={'file':song.get('file'),'track':song.get('track')}
+# print(identity)
 print(song)
-#delete song
+# delete song
 client.deleteid(song['id'])
 util.closeClient(client)
