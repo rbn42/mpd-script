63bb6420490363b8a2e3d6b3d1a9141a80a1409e
rbn42 <bl100@students.waikato.ac.nz>
2016-12-05 09:46:14 +1300

    favfilter.py

--- favfilter.py
+++ favfilter.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 import os.path
 
-KEYS = 'file', 'time', 'title', 'artist', 'album', 'track'  # 'pos'
+KEYS = 'time', 'title', 'artist', 'album', 'track'  # 'pos'
 _data = []
 
 
 def parse2str(item):
+    # 2016-12-05 09:44:13 Mon NZDT
+    # path取末尾两级,这样移动文件,改变目录结构的时候可以提供容错
+    # 因为有time标记做保险,即使有重名文件存在,撞车的概率应该也不会很大
+    path = item['file']
+    path = '/'.join(path.split('/')[-2:])
+
     keys = [k for k in item if k in KEYS]
     keys.sort()
-    values = [item[k] for k in keys]
+    values = [path] + [item[k] for k in keys]
     return str(values)
     _str = ','.join(values)
     return _str
