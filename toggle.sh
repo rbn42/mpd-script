2ea5e8989e992dadf87721bb0ae4ae9630a42539
rbn42 <bl100@students.waikato.ac.nz>
2017-03-14 18:51:13 +1300

    notify

--- toggle.sh
+++ toggle.sh
@@ -9,4 +9,17 @@ print(state)
 """
 export MSG2=`python -c "$SCRIPT" "$MSG"`
 echo $MSG2
-notify-send "$MSG2"
+
+#notify-send "$MSG2"
+
+
+#暂且没有好的指示ui,就用vol来顶替了
+#notify本身不够好,因为toggle多按几次,旧消息不会消失,就会搞不清播放状态了
+if [ "[playing]" == $MSG2 ];
+then
+    volnoti-show 100 
+else
+    volnoti-show 0 -m
+fi
+
+
